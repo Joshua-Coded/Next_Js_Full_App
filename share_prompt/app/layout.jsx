4570552099932ccc13e0b80f2c33b38db3d48865
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-06-10 02:26:18 +0200

    higher order function

--- share_prompt/app/layout.jsx
+++ share_prompt/app/layout.jsx
@@ -11,14 +11,16 @@ export const metadata = {
 const RootLayout = ({ children }) => (
   <html lang="en">
     <body>
-      <div className="main">
-        <div className="gradient" />
-      </div>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
 
-      <main className="app">
-        <Nav />
-        {children}
-      </main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
     </body>
   </html>
 );
