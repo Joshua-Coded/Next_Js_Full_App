54a99cd684fa41efffba5e2e9c4ce2d975047812
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-06-16 03:08:01 +0200

    home page done

--- share_prompt/app/api/auth/[...nextauth]/route.js
+++ share_prompt/app/api/auth/[...nextauth]/route.js
@@ -13,36 +13,40 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,  
         })
     ],
-    async session ({ session }) {
-const sessionUser = await User.findOne({
-    email: session.user.email
-})
-        session.user.id = sessionUser._id.toString();
 
-        return session;
-    },
-
-    async signIn({ profile}) {
-        try {
-            await connectToDB();
-
-            // check if the user is already existing
-            const userExists = await User.findOne({ email: profile.email });
-
-            // if not, create a new user
-
-            if (!userExists) {
-                await User.create({ 
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                image: profile.picture
-            });
-            }
-            return true;
-        } catch (error) {
-            console.log(error);            
-        }
-    }
+    callbacks: { 
+        async session ({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
+                    session.user.id = sessionUser._id.toString();
+            
+                    return session;
+                },
+            
+                async signIn({ profile}) {
+                    try {
+                        await connectToDB();
+            
+                        // check if the user is already existing
+                        const userExists = await User.findOne({ email: profile.email });
+            
+                        // if not, create a new user
+            
+                        if (!userExists) {
+                            await User.create({ 
+                                email: profile.email,
+                                username: profile.name.replace(" ", "").toLowerCase(),
+                            image: profile.picture
+                        });
+                        }
+                        return true;
+                    } catch (error) {
+                        console.log(error);            
+                    }
+                }
+     }
+   
 })
 
 export { handler as GET, handler as POST };
