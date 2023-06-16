618b78536bcc4a46f5774a6482e969cc2afc8c7d
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-06-16 02:35:46 +0200

    user route

--- share_prompt/app/api/auth/[...nextauth]/route.js
+++ share_prompt/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 
@@ -12,7 +14,12 @@ const handler = NextAuth({
         })
     ],
     async session ({ session }) {
+const sessionUser = await User.findOne({
+    email: session.user.email
+})
+        session.user.id = sessionUser._id.toString();
 
+        return session;
     },
 
     async signIn({ profile}) {
@@ -20,10 +27,17 @@ const handler = NextAuth({
             await connectToDB();
 
             // check if the user is already existing
-
+            const userExists = await User.findOne({ email: profile.email });
 
             // if not, create a new user
 
+            if (!userExists) {
+                await User.create({ 
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                image: profile.picture
+            });
+            }
             return true;
         } catch (error) {
             console.log(error);            
