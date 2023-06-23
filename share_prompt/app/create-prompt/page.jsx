1c283a922db51bce38e67765d5ee5c8c46f32216
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-06-23 13:08:08 +0200

    form for prompt almost

--- share_prompt/app/create-prompt/page.jsx
+++ share_prompt/app/create-prompt/page.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  return <div>Create Prompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    post: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {};
+
+  return (
+    <Form
+      type="Create Prompt"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
