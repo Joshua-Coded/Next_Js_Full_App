1c4286a81c16804412360fd30d6bed2c8e9a4ad0
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-06-23 13:25:03 +0200

    front end donefor form

--- share_prompt/app/create-prompt/page.jsx
+++ share_prompt/app/create-prompt/page.jsx
@@ -13,7 +13,30 @@ const CreatePrompt = () => {
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tags: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
