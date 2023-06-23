a2a294b81b85ee8dae5f8a4f387e7a2a10c6de04
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-06-23 13:49:20 +0200

    up next is populating the feeds after creating them

--- share_prompt/app/create-prompt/page.jsx
+++ share_prompt/app/create-prompt/page.jsx
@@ -7,6 +7,9 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     post: "",
