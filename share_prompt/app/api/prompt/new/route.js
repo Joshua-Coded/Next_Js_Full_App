a68995cfa091c45f40daa70f9ab59e3f7c73d8a5
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-06-23 13:43:50 +0200

    prompt creation done

--- share_prompt/app/api/prompt/new/route.js
+++ share_prompt/app/api/prompt/new/route.js
@@ -1,10 +1,16 @@
 import { connectToDB } from "@utils/database";
-
+import Prompt from "@models/prompt";
 
 export const POST = async (req) => {
     const {userId, prompt, tag} = await req.json();
 
     try {
         await connectToDB();
-    } catch (err) {}
+        const newPrompt = new Prompt({creator: userId, prompt, tag})
+        await newPrompt.save();
+
+        return new Response(JSON.stringify(newPrompt), {status: 201});
+    } catch (err) {
+        return new Response("Failed to create prompt", {status:500});
+    }
 };
