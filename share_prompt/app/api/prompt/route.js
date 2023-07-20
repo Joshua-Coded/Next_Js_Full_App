5e2b2f9fe06d99f7cee1bad3ac83994d44dd9dcb
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-20 23:26:03 +0200

    api configured

---
+++ share_prompt/app/api/prompt/route.js
@@ -0,0 +1,18 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+
+
+export const GET = async (request) => {
+    try {
+await connectToDB();
+
+const prompts = await Prompt.find({}).populate('creator');
+
+return new Response(JSON.stringify(prompts), { status: 200});
+    }
+    catch (err) {
+
+        return new Response("Failed to fetch all prompts", { status: 500});
+    }
+}
