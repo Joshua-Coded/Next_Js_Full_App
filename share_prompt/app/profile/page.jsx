9494530ad94360d0ebfd0cf0acf22c486a752cdd
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-21 01:05:44 +0200

    profile page done

--- share_prompt/app/profile/page.jsx
+++ share_prompt/app/profile/page.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const Profile = () => {
-  return <div>page</div>;
+import Profile from "@components/profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  return (
+    <div>
+      <Profile
+        name="My Profile"
+        desc="Welcome to your personalized profile page"
+        data={posts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
 };
 
-export default Profile;
+export default MyProfile;
