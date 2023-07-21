e0a1547c4471eea70f925a3fcd46158050223ccf
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-21 02:17:24 +0200

    update crud done

--- share_prompt/app/profile/page.jsx
+++ share_prompt/app/profile/page.jsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
+
   const { data: session } = useSession();
 
   const [posts, setPosts] = useState([]);
@@ -20,9 +22,11 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {};
 
   return (
     <div>
